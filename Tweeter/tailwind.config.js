79917568442c05525d4b3f793c8b52e38568b855
isakjensen <isakludvigjensen@gmail.com>
2022-02-11 11:52:04 +0100

    jsx twc

--- Tweeter/tailwind.config.js
+++ Tweeter/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 
     "./resources/js/components/*.js",
     "./resources/js/components/**/*.js",
+
+    "./resources/js/routes/*.jsx",
+    "./resources/js/routes/**/*.jsx",
   ],
   theme: {
     extend: {},
