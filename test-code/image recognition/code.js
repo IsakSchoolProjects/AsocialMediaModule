e1f4a46a0249c12845558d1bbe812d7902c93658
Alexander Karlsson <alexander.karlsson@learnet.se>
2022-02-14 09:22:32 +0100

    having problems

--- test-code/image recognition/code.js
+++ test-code/image recognition/code.js
@@ -1,5 +1,24 @@
-// import { read } from "jimp";
 const Image = document.getElementById("image");
+const Canvas = document.getElementById("canvas");
+
+let clr = "rgba(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + (Math.round(Math.random() * 100) / 100 + .2) + ")";
+let fillClr = ["rgba(250,225,6,0.5)", "rgba(0,0,0,10)", clr];
+
+function download(dataurl, filename) {
+    const link = document.createElement("a");
+    link.href = dataurl;
+    link.download = filename;
+    link.click();
+}
+
+function dataURLtoFile(dataUrl, fileName) {
+    var arr = dataUrl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], fileName, { type: mime });
+}
 
 async function findFaces() {
     const model = await blazeface.load();
@@ -12,13 +31,16 @@ async function findFaces() {
         canvas.width = img.width;
         canvas.height = img.height;
         const ctx = canvas.getContext("2d");
-        ctx.fillStyle = "rgba(250,225,6,0.5)";
+        ctx.fillStyle = fillClr[1];
         for (let i = 0; i < predictions.length; i++) {
             const start = predictions[i].topLeft;
             const end = predictions[i].bottomRight;
             const size = [end[0] - start[0], end[1] - start[1]];
             ctx.fillRect(start[0], start[1], size[0], size[1]);
         }
+        let newImage = Canvas.toDataURL("image/png");
+        let fileReader = new FileReader();
+        mergeImages([Image.src, dataURLtoFile(newImage, "name")]).then(b64 => download(b64, "test"));
     } else {
         document.getElementById("status").innerText = "No Face(s) Found";
     }
@@ -26,4 +48,6 @@ async function findFaces() {
 
 findFaces();
 
-// read(Image.src).then();
+
+
+// Jimp.read(Image.src).then();
